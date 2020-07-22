bfc833cb7e1853ba554b24fd9cd559c451d7a941
unknown <chittirajendra777@gmail.com>
2020-07-22 16:27:34 +0530

    Use snowball stemmer to find new types

--- RDIP_1/Codes/JS/script.js
+++ RDIP_1/Codes/JS/script.js
@@ -1,13 +1,14 @@
-
 var requirement1=['A‌ ‌mouse‌ ‌was‌ ‌having‌ ‌a‌ ‌very‌ ‌bad‌ ‌time.‌ ‌She‌ ‌could‌ ‌find‌ ‌no‌ ‌food‌ ‌at‌ ‌all.‌ ‌She‌ ‌looked‌ ‌here‌ ‌and‌ ‌there,‌ ‌but‌ ‌there‌ ‌was‌ ‌no‌ ‌food,‌ ‌and‌ ‌she‌ ‌grew‌ ‌very‌ ‌thin.‌ ‌At‌ ‌last‌ ‌the‌ ‌mouse‌ ‌found‌ ‌a‌ ‌basket,‌ ‌full‌ ‌of‌ ‌corn.‌ ‌There‌ ‌was‌ ‌a‌ ‌small‌ ‌hole‌ ‌in‌ ‌the‌ ‌basket,‌ ‌and‌ ‌she‌ ‌crept‌ ‌in.‌ ‌She‌ ‌could‌ ‌just‌ ‌get‌ ‌through‌ ‌the‌ ‌hole.‌ ‌Then‌ ‌she‌ ‌began‌ ‌to‌ ‌eat‌ ‌the‌ ‌corn.‌ ‌Being‌ ‌very‌ ‌hungry,‌ ‌she‌ ‌ate‌ ‌a‌ ‌great‌ ‌deal,‌ ‌and‌ ‌went‌ ‌on‌ ‌eating‌ ‌and‌ ‌eating.‌ ‌She‌ ‌had‌ ‌grown‌ ‌very‌ ‌fat‌ ‌before‌ ‌she‌ ‌felt‌ ‌that‌ ‌she‌ ‌had‌ ‌had‌ ‌enough.‌ ‌When‌ ‌the‌ ‌mouse‌ ‌tried‌ ‌to‌ ‌climb‌ ‌out‌ ‌of‌ ‌the‌ ‌basket,‌ ‌she‌ ‌could‌ ‌not.‌ ‌She‌ ‌was‌ ‌too‌ ‌fat‌ ‌to‌ ‌pass‌ ‌through‌ ‌the‌ ‌hole.‌ ‌"How‌ ‌shall‌ ‌I‌ ‌climb‌ ‌out?"‌ ‌said‌ ‌the‌ ‌mouse.‌ ‌"oh,‌ ‌how‌ ‌shall‌ ‌I‌ ‌climb‌ ‌out?"‌ ‌Just‌ ‌then‌ ‌a‌ ‌rat‌ ‌came‌ ‌along,‌ ‌and‌ ‌he‌ ‌heard‌ ‌the‌ ‌mouse.‌ ‌"Mouse,"‌ ‌said‌ ‌the‌ ‌rat,‌ ‌"if‌ ‌you‌ ‌want‌ ‌to‌ ‌climb‌ ‌out‌ ‌of‌ ‌the‌ ‌basket,‌ ‌you‌ ‌must‌ ‌wait‌ ‌till‌ ‌you‌ ‌have‌ ‌grown‌ ‌as‌ ‌thin‌ ‌as‌ ‌you‌ ‌were‌ ‌when‌ ‌you‌ ‌went‌ ‌in.‌'];
 var requirement2=['A‌ ‌wolf‌ ‌carried‌ ‌off‌ ‌a‌ ‌lamb.‌ ‌The‌ ‌lamb‌ ‌said,‌ ‌"‌ ‌I‌ ‌know‌ ‌you‌ ‌are‌ ‌going‌ ‌to‌ ‌eat‌ ‌me,‌ ‌but‌ ‌before‌ ‌you‌ ‌eat‌ ‌me‌ ‌I‌ ‌would‌ ‌like‌ ‌to‌ ‌hear‌ ‌you‌ ‌play‌ ‌the‌ ‌flute.‌ ‌I‌ ‌have‌ ‌heard‌ ‌that‌ ‌you‌ ‌can‌ ‌play‌ ‌the‌ ‌flute‌ ‌better‌ ‌than‌ ‌anyone‌ ‌else,‌ ‌even‌ ‌the‌ ‌shepherd‌ ‌himself."‌ ‌The‌ ‌wolf‌ ‌was‌ ‌so‌ ‌pleased‌ ‌at‌ ‌this‌ ‌that‌ ‌he‌ ‌took‌ ‌out‌ ‌his‌ ‌flute‌ ‌and‌ ‌began‌ ‌to‌ ‌play.‌ ‌When‌ ‌he‌ ‌had‌ ‌done,‌ ‌the‌ ‌lamb‌ ‌insisted‌ ‌him‌ ‌to‌ ‌play‌ ‌once‌ ‌more‌ ‌and‌ ‌the‌ ‌wolf‌ ‌played‌ ‌again.‌ ‌The‌ ‌shepherd‌ ‌and‌ ‌the‌ ‌dogs‌ ‌heard‌ ‌the‌ ‌sound,‌ ‌and‌ ‌they‌ ‌came‌ ‌running‌ ‌up‌ ‌and‌ ‌fell‌ ‌on‌ ‌the‌ ‌wolf‌ ‌and‌ ‌the‌ ‌lamb‌ ‌was‌ ‌able‌ ‌to‌ ‌get‌ ‌back‌ ‌to‌ ‌the‌ ‌flock.‌'];
 var requirement3=['A‌ ‌man‌ ‌had‌ ‌a‌ ‌little‌ ‌dog,‌ ‌and‌ ‌he‌ ‌was‌ ‌very‌ ‌fond‌ ‌of‌ ‌it.‌ ‌He‌ ‌would‌ ‌pat‌ ‌its‌ ‌head,‌ ‌and‌ ‌take‌ ‌it‌ ‌on‌ ‌his‌ ‌knee,‌ ‌and‌ ‌talk‌ ‌to‌ ‌it.‌ ‌Then‌ ‌he‌ ‌would‌ ‌give‌ ‌it‌ ‌little‌ ‌bits‌ ‌of‌ ‌food‌ ‌from‌ ‌his‌ ‌own‌ ‌plate.‌ ‌A‌ ‌donkey‌ ‌looked‌ ‌in‌ ‌at‌ ‌the‌ ‌window‌ ‌and‌ ‌saw‌ ‌the‌ ‌man‌ ‌and‌ ‌the‌ ‌dog.‌ ‌"Why‌ ‌does‌ ‌he‌ ‌not‌ ‌make‌ ‌a‌ ‌pet‌ ‌of‌ ‌me?"‌ ‌said‌ ‌the‌ ‌donkey.‌ ‌"It‌ ‌is‌ ‌not‌ ‌fair.‌ ‌I‌ ‌work‌ ‌hard,‌ ‌and‌ ‌the‌ ‌dog‌ ‌only‌ ‌wags‌ ‌its‌ ‌tail,‌ ‌and‌ ‌barks,‌ ‌and‌ ‌jumps‌ ‌on‌ ‌its‌ ‌masters‌ ‌knee.‌ ‌It‌ is ‌not‌ ‌fair."‌ ‌Then‌ ‌the‌ ‌donkey‌ ‌said‌ ‌to‌ ‌himself,‌ ‌"If‌ ‌I‌ ‌do‌ ‌what‌ ‌the‌ ‌dog‌ ‌does,‌ ‌he‌ ‌may‌ ‌make‌ ‌a‌ ‌pet‌ ‌of‌ ‌me."‌ ‌So‌ ‌the‌ ‌donkey‌ ‌ran‌ ‌into‌ ‌the‌ ‌room.‌ ‌It‌ ‌brayed‌ ‌as‌ ‌loudly‌ ‌as‌ ‌it‌ ‌could.‌ ‌It‌ ‌wagged‌ ‌its‌ ‌tail‌ ‌so‌ ‌hard‌ ‌that‌ ‌it‌ ‌knocked‌ ‌over‌ ‌a‌ ‌jar‌ ‌on‌ ‌the‌ ‌table.‌ ‌Then‌ ‌it‌ ‌tried‌ ‌to‌ ‌jump‌ ‌on‌ ‌to‌ ‌its‌ ‌masters‌ ‌knee.‌ ‌The‌ ‌master‌ ‌thought‌ ‌the‌ ‌donkey‌ ‌was‌ ‌mad,‌ ‌and‌ ‌he‌ ‌shouted,‌ ‌"Help!‌ ‌Help!"‌ ‌Men‌ ‌came‌ ‌running‌ ‌in‌ ‌with‌ ‌sticks,‌ ‌and‌ ‌they‌ ‌beat‌ ‌the‌ ‌donkey‌ ‌till‌ ‌it‌ ‌ran‌ ‌out‌ ‌of‌ ‌the‌ ‌house,‌ ‌and‌ ‌they‌ ‌drove‌ ‌it‌ ‌back‌ ‌to‌ ‌the‌ ‌field.‌ ‌"I‌ ‌only‌ ‌did‌ ‌what‌ ‌the‌ ‌dog‌ ‌does,"‌ ‌said‌ ‌the‌ ‌donkey,"‌ ‌and‌ ‌yet‌ ‌they‌ ‌make‌ ‌a‌ ‌pet‌ ‌of‌ ‌the‌ ‌dog,‌ ‌and‌ ‌they‌ ‌beat‌ ‌me‌ ‌with‌ ‌sticks.‌ ‌It‌ ‌is‌ ‌not‌ ‌fair.‌'];
 
 var wordlength=0;
+var root=0;
+
 var stemmer = new Snowball('English');
-stemmer.setCurrent('tell');
+stemmer.setCurrent('abbreviations');      /* Word given to code as abbreviations */ 
 stemmer.stem();
-console.log(stemmer.getCurrent());
+console.log(stemmer.getCurrent());    
 
 function selectcorpus(){
    
@@ -30,6 +31,7 @@ function selectcorpus(){
         document.getElementById("require-text").innerHTML=requirement2;
         document.getElementById("msg").innerHTML="Enter the number of tokens and types for the above corpus:"
         document.getElementById('ttable').style.visibility="visible";
+       
         btng();
     }
     else if(document.getElementById("corpus3").selected)
@@ -49,6 +51,7 @@ function count(){
    
     var c=document.getElementById("require-text").innerHTML;
     var wordcount=c.split(' ').length;
+  
      var check=document.getElementById("wfield").value;
      var c=document.getElementById("require-text").innerHTML;
      d=c.toLowerCase();
@@ -57,6 +60,7 @@ function count(){
 
      
     wordlength=new Set(d.split(' ')).size;;
+    
      
      var e=document.getElementById("tfield").value;
      
@@ -97,5 +101,27 @@ function process(){
     document.getElementById("btn").style.visibility="hidden";
     document.getElementById("disp1").innerHTML="Now, consider all the tokens with the same 'root' word to be of the same type. Recalculate the number of types.";
     document.getElementById("rootmsg").innerHTML="#newtypes:"+"<br>"+"<input type='text' id='rootfield'>"
-    document.getElementById("final").innerHTML="<button type='button' id='finalsubmit'>Submit</button>"
+    document.getElementById("final").innerHTML="<button type='button' id='finalsubmit' onclick='checkroot()'>Submit</button>"
+}
+function checkroot(){
+    if((document.getElementById("corpus1").selected) || (document.getElementById("corpus2").selected) || (document.getElementById("corpus3").selected))
+    {
+    var c=document.getElementById("require-text").innerHTML;
+    var d=c.toLowerCase();
+     d=d.replace(/[".?,]+/g , '');
+     d=d.replace(/\s\s+/g, ' ');
+     root=new Set(d.split(' ')).size;
+     var types=[];
+     for(var i=0;i<=(root.length-1);i++)
+     {
+        stemmer.setCurrent(types[i]);
+        stemmer.stem();
+        types.push(stemmer.getCurrent());
+     }
+     
+    }
+   
+
+   
+  
 }
